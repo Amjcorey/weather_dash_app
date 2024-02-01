f92052a8e0b0e70892cc1c176df6b9eca5636076
Amanda Corey <76081915+Amjcorey@users.noreply.github.com>
2024-01-31 21:13:48 -0800

    modify footer and main element in html

--- src/weather.js
+++ src/weather.js
@@ -1,68 +1,62 @@
 //Refreshed weather data
 function refreshWeather(response) {
-    let temperatureElement = document.querySelector("#temperature");
-    let temperature = response.data.temperature.current;
-    let cityElement = document.querySelector("#city");
-    let timeElement = document.querySelector("#time");
-    let date = new Date(response.data.time * 1000);
-    let iconElement = document.querySelector("#icon");
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
-//Elements
-cityElement.innerHTML = response.data.city;
-    timeElement.innerHTML = formatDate(date);
-    temperatureElement.innerHTML = Math.round(temperature);
-    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+  //Elements
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
-
-
-
 
 // Local time
 
-
 function formatDate(date) {
-    let minutes = date.getMinutes();
-    let hours = date.getHours();
-    let days = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday"
-    ];
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-    let day = days[date.getDay()];
+  let day = days[date.getDay()];
 
-    if (minutes < 10) {
-        minutes = `0${minutes}`;
-    }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-    return `${day}, ${hours}:${minutes}`;
+  return `${day}, ${hours}:${minutes}`;
 }
-
 
 function searchCity(city) {
-    let apiKey  = "4ba3572496537oebd26f86a5bb6t80fa";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
- console.log(apiUrl);
-    axios.get(apiUrl).then(refreshWeather);
+  let apiKey = "4ba3572496537oebd26f86a5bb6t80fa";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
 }
 
-
 function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-console.log(searchInput.value);
-    searchCity(searchInput.value);
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  console.log(searchInput.value);
+  searchCity(searchInput.value);
 }
 
 //Get time stamp
 function formatDay(timestamp) {
-    let date = new Date(timestamp * 1000);
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    return days[date.getDay()];
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
 }
 
 let searchFormElement = document.querySelector("#search-form");
