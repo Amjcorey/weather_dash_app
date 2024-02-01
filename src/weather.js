69fc5e4a0d83ccd3e4446277c20bdcdba2239b29
Amanda Corey <76081915+Amjcorey@users.noreply.github.com>
2024-01-31 19:06:14 -0800

    Begin js date/time function

--- src/weather.js
+++ src/weather.js
@@ -2,6 +2,48 @@
 
 
 
+
+// Local time
+let currentTime = new Date();
+
+function formateDate(date) {
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+
+    let currentYear = date.getFullYear();
+    let currentDay = days[date.getDay()];
+    let currentMonth = months[date.getMonth()];
+    let currentDate = date.getDate();
+
+    let formattedDate = `Date: ${currentDay}, ${currentMonth} ${currentDate}, ${currentYear}`;
+
+    return formattedDate();
+
+}
+
+
 function searchCity(city) {
     let apiKey  = "XX";
     let apiUrl = "xxx";
