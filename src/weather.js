28c56e259fe9d544dda57522695f5429c58e4d22
Amanda Corey <76081915+Amjcorey@users.noreply.github.com>
2024-04-21 19:31:09 -0700

    comment out some code in html, will work on update. Add openweather api and fetch call in script.

--- src/weather.js
+++ src/weather.js
@@ -15,34 +15,54 @@ function refreshWeather(response) {
 }
 
 // Local time
+// function formatDate(date) {
+//   let minutes = date.getMinutes();
+//   let hours = date.getHours();
+//   let days = [
+//     "Sunday",
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday",
+//   ];
 
-function formatDate(date) {
-  let minutes = date.getMinutes();
-  let hours = date.getHours();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+//   let day = days[date.getDay()];
 
-  let day = days[date.getDay()];
+//   if (minutes < 10) {
+//     minutes = `0${minutes}`;
+//   }
 
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
+//   return `${day}, ${hours}:${minutes}`;
+// }
 
-  return `${day}, ${hours}:${minutes}`;
-}
-
-function searchCity(city) {
-  let apiKey = "4ba3572496537oebd26f86a5bb6t80fa";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+function fetchLocalation(city) {
+  let city = city.trim().toLowerCase();
+  let apiKey = '0968472c2bf24b729bf6206c08f6fed2';
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=
+  ${lon}&appid=${apiKey}`;
   console.log(apiUrl);
-  axios.get(apiUrl).then(refreshWeather);
+  // axios.get(apiUrl).then(refreshWeather);
+fetch(apiUrl).then((response) => {
+  if (!response.ok) {
+    throw new Error("Cannot find that city");
+  }
+  return response.json();
+})
+.then((data) => {
+  if ( data && data.length > 0 ) {
+    let { lat, lon } = data[0];
+    fetchLocalWeather( city, lat, lon );
+  } 
+  else {
+    displayError("Cannot find that city");
+  }
+})
+.catch((error) => {
+  console.error("Error: ", error);
+  displayError("There was an error while retrieving that information...");
+});
 }
 
 function handleSearchSubmit(event) {
