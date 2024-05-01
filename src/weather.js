8901ea054bc2074e9d6459cdafe6b74f1d638dd3
Amanda Corey <76081915+Amjcorey@users.noreply.github.com>
2024-05-01 16:43:27 -0700

    Update weather.js with search code. Use geo API from Open Weather this time.

--- src/weather.js
+++ src/weather.js
@@ -1,21 +1,25 @@
 let inputCity = document.querySelector(".form-control");
 let searchButton = document.querySelector(".search-form-button");
+let apiKey = "0968472c2bf24b729bf6206c08f6fed2";
 
-const getCityCoordinates = () => {
+let getCityCoordinates = () => {
   let cityName = inputCity.value.trim();
   if(!cityName) return; // Return if search form is empty
+  let geoApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`;
 
+  console.log(geoApiUrl);
+
+  fetch(geoApiUrl).then(res => res.json()).then( data => {
+    console.log(data);
+  }).catch(() => {
+    alert("Uh oh! An error occured while fetching those coordinates.");
+  });
   // console.log(cityName);
 }
 
 searchButton.addEventListener("click", getCityCoordinates);
 
 
-
-
-// function fetchLocalation() {
-
-//   let apiKey = '0968472c2bf24b729bf6206c08f6fed2';
+//   
 //   let apiUrl = `https://api.openweathermap.org/geo/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 //   console.log(apiUrl);
-// }
