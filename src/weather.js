f49c1b7f415fb7928d066d132a79f5409241c30a
Amanda Corey <76081915+Amjcorey@users.noreply.github.com>
2024-05-01 19:13:34 -0700

    add code to allow user to search city weather, temp, wind, and humidity. add units to api URL. add code to create  forecast cards. City search is not loading, getting error alert that coordinates aren't found but no error message in console.

--- src/weather.js
+++ src/weather.js
@@ -1,28 +1,49 @@
 let inputCity = document.querySelector(".form-control");
 let searchButton = document.querySelector(".search-form-button");
+let currentWeatherDiv = document.querySelector(".current-weather");
+let weatherCardDiv = document.querySelector(".forecast-cards");
 
 // My api key
 let apiKey = "0968472c2bf24b729bf6206c08f6fed2";
 
-let createWeatherCard = (weatherItem) => {
-  return`                    
+let createWeatherCard = (cityName, weatherItem, index) => {
+  if (index === 0) {
+    // HTML for the main weather display
+    `
+<div class="weather-overview">
+                    <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
+                    <h4>Temperature ${weatherItem.main.temp} F</h4>
+                    <h4>High ${weatherItem.main.temp_max} | Low ${weatherItem.main.temp_min}</h4>
+                    <h4>Wind: ${weatherItem.wind.speed}MPS</h4>
+                    <h4>Humidity: ${weatherItem.main.humidity}%</h4>
+                </div>
+                <div class="weather-icon">
+                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon
+                  }@4x.png" alt="weather-icon">
+                    <h4>${weatherItem.weather[0].description}</h4>
+                </div>
+`;
+  } else {
+    // HTML for five day forecast
+    return `                    
   <li class="card">
-  <h3>04/22/2024</h3>
-  <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="weather-icon">
-  <h4>Temp: </h4>
-  <h4>Wind: mps</h4>
-  <h4>Humidity: %</h4>
+  <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+  <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon
+      }@2x.png" alt="weather-icon">
+  <h4>Temp: ${weatherItem.main.temp}</h4>
+  <h4>H:${weatherItem.main.temp_max} | L:${weatherItem.main.temp_min}</h4>
+  <h4>Wind: ${weatherItem.wind.speed}MPS</h4>
 </li>
-`
-}
+`;
+  }
+};
 
 let getWeatherInformation = (cityName, lat, lon) => {
-  let apiUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  let apiUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
 
   fetch(apiUrl)
     .then((res) => res.json())
     .then((data) => {
-
       // Filters the forecast to get only one forecast per day
       let forecastDays = [];
       let fiveDayForecast = data.list.filter((forecast) => {
@@ -31,10 +52,27 @@ let getWeatherInformation = (cityName, lat, lon) => {
           return forecastDays.push(forecastDate);
         }
       });
-      console.log(fiveDayForecast);
-      fiveDayForecast.forEach(weatherItem => {
-        createWeatherCard(weatherItem);
-      })
+
+      //Clear previous weather data
+      inputCity.value = "";
+      currentWeatherDiv.innerHTML = "";
+      weatherCardDiv.innerHTML = "";
+
+      // console.log(fiveDayForecast);
+      //Adding weather cards to DOM
+      fiveDayForecast.forEach(weatherItem, (index) => {
+        if (index === 0) {
+          currentWeatherDiv.insertAdjacentHTML(
+            "beforeend",
+            createWeatherCard(cityName, weatherItem, index)
+          );
+        } else {
+          weatherCardDiv.insertAdjacentHTML(
+            "beforeend",
+            createWeatherCard(cityName, weatherItem, index)
+          );
+        }
+      });
     })
     .catch(() => {
       alert("Uh oh! An error occured while fetching the weather forecast.");
@@ -45,7 +83,7 @@ let getWeatherInformation = (cityName, lat, lon) => {
 let getCityCoordinates = () => {
   let cityName = inputCity.value.trim();
   if (!cityName) return; // Return if search form is empty
-  let geoApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`;
+  let geoApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}&units=imperial`;
 
   // console.log(geoApiUrl);
 
